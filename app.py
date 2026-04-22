32c4bb2118ad611aad43829a0fa0fd020c95ba9a
GabriellaJovanka <142808681+GabriellaJovanka@users.noreply.github.com>
2026-04-22 12:27:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ st.markdown("""
 
         div[data-testid="stFormSubmitButton"] button:hover {
             background-color: #f7e6d2 !important;
-            border-color: #355872 !important;
+            border-color: #FFFFFF !important;
             color: #000000 !important;
             transform: scale(1.01);
         }
