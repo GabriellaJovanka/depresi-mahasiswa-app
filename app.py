84ee2f49c2ae954d585a74cd03af3b91638fc03f
GabriellaJovanka <142808681+GabriellaJovanka@users.noreply.github.com>
2026-04-22 12:19:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ st.markdown("""
 
         /* TOMBOL ANALISIS */
         div[data-testid="stFormSubmitButton"] button {
-            background-color: #F4F0E4 !important;
+            background-color: #355872 !important;
             color: #355872 !important;
             border: 2px solid #EFD2B0 !important;
             font-weight: bold;
